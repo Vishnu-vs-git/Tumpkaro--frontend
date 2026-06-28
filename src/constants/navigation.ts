ab45d9df9bd5f0f94163e598b4ec877341c62dc2
vishnu <getvishnuvs@gmail.com>
2026-06-28 11:43:07 +0530

    added navbar

---
+++ src/constants/navigation.ts
@@ -0,0 +1,27 @@
+export interface NavLink {
+  label: string;
+  href: string;
+}
+
+export const navLinks: NavLink[] = [
+  {
+    label: "Features",
+    href: "#features",
+  },
+  {
+    label: "Templates",
+    href: "#templates",
+  },
+  {
+    label: "Pricing",
+    href: "#pricing",
+  },
+  {
+    label: "Showcase",
+    href: "#showcase",
+  },
+  {
+    label: "FAQ",
+    href: "#faq",
+  },
+];
